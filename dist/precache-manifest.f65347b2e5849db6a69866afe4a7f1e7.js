5f430bfb6b63b041751813ab42b2ca3aa6267096
Deva Jayantha <devajayantha870@gmail.com>
2020-10-29 15:43:49 +0800

    update2

---
+++ dist/precache-manifest.f65347b2e5849db6a69866afe4a7f1e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f544cb8a79ae6df17c3e",
+    "url": "/css/app.8607edc3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d4a8930c6175d614f02bdaa5b92e0d02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4fa6e24b9ece02c576e",
+    "url": "/js/about.0eefeff3.js"
+  },
+  {
+    "revision": "f544cb8a79ae6df17c3e",
+    "url": "/js/app.5c9e28c1.js"
+  },
+  {
+    "revision": "cb82d6dee2ff633ad033",
+    "url": "/js/chunk-vendors.12f5b8e8.js"
+  },
+  {
+    "revision": "30af9b6d65e2e9ecd57198fef9f2b3ee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
